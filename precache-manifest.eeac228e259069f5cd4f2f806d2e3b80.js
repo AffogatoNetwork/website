92726e22d4106a76743619e57f4aba4178bb776d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-27 20:05:20 +0000

    Deploy affogatonetwork.com to github.com/AffogatoNetwork/website.git:gh-pages

---
+++ precache-manifest.eeac228e259069f5cd4f2f806d2e3b80.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc2740594c5733edb85a",
+    "url": "/static/css/main.d3e05e23.chunk.css"
+  },
+  {
+    "revision": "bc2740594c5733edb85a",
+    "url": "/static/js/main.bc274059.chunk.js"
+  },
+  {
+    "revision": "c3e8738626f69d0c4690",
+    "url": "/static/css/1.de04685f.chunk.css"
+  },
+  {
+    "revision": "c3e8738626f69d0c4690",
+    "url": "/static/js/1.c3e87386.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9719cf286928521e948ed9acd5c685bd",
+    "url": "/static/media/logo.9719cf28.png"
+  },
+  {
+    "revision": "3aad28e226933f251be5c80a46c88de4",
+    "url": "/static/media/cristian.3aad28e2.jpg"
+  },
+  {
+    "revision": "6028eb9de5b4bc82f32f891775c4ca09",
+    "url": "/static/media/daniel.6028eb9d.jpg"
+  },
+  {
+    "revision": "12279f61e9777186c5ad73e6a4b1132a",
+    "url": "/static/media/robert.12279f61.jpeg"
+  },
+  {
+    "revision": "ff6afd65c6d1245a4d8e2a6ad6554bf3",
+    "url": "/static/media/oscar.ff6afd65.jpeg"
+  },
+  {
+    "revision": "dcc631ef8398b6696f6051416d4de9d7",
+    "url": "/static/media/nestor.dcc631ef.jpeg"
+  },
+  {
+    "revision": "010ce775d3073d6371698008e598f289",
+    "url": "/static/media/jorge.010ce775.jpg"
+  },
+  {
+    "revision": "204fc0cf4b31fab091bf50cf13838306",
+    "url": "/static/media/phone.204fc0cf.svg"
+  },
+  {
+    "revision": "7b1bc49198bcb3c271db37592243e228",
+    "url": "/static/media/case1.7b1bc491.jpg"
+  },
+  {
+    "revision": "05d10c8e7003bd00c531783709fe68f7",
+    "url": "/static/media/case2.05d10c8e.jpg"
+  },
+  {
+    "revision": "5d2999f8c647ae4c6b92ee1885e2111d",
+    "url": "/static/media/case3.5d2999f8.jpg"
+  },
+  {
+    "revision": "14edbdf7911058b480cbed5695766d85",
+    "url": "/static/media/proexo.14edbdf7.png"
+  },
+  {
+    "revision": "e0167b07ae88b273c2315ba351900c1d",
+    "url": "/static/media/logo_DOMarcala.e0167b07.jpg"
+  },
+  {
+    "revision": "4cef2ad7736c2c53e104b55448c2edad",
+    "url": "/static/media/logo_Nativo.4cef2ad7.jpg"
+  },
+  {
+    "revision": "149bdb81eb83f82ab42f672bc3acd7cf",
+    "url": "/static/media/logo_Adelante.149bdb81.png"
+  },
+  {
+    "revision": "8079cc09d70cbb93355819cae99e6c61",
+    "url": "/static/media/galeano.8079cc09.jpg"
+  },
+  {
+    "revision": "ecb49a7dabd668315996c934a71c5341",
+    "url": "/static/media/bid.ecb49a7d.png"
+  },
+  {
+    "revision": "9de70df9778de7e0233e07302fffd33b",
+    "url": "/static/media/rainforest.9de70df9.png"
+  },
+  {
+    "revision": "4dd075f4135fb91f28313d9c9968e7a3",
+    "url": "/index.html"
+  }
+];
